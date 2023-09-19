4a44a372b23399b557d4cda5aa740a153cca3d5f
Pr Things <prthings@skiff.com>
2023-09-19 22:50:50 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-
 export default function App() {
   return (
     <div>
+      <div className='home'>
       <h1>Hello Home</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ut in atque, maxime incidunt, exercitationem quisquam consequatur odit sequi sapiente repellat debitis harum sit ab accusantium earum dolor facere rem.</p>
+      <button>Start</button>
+      </div>
     </div>
   )
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {Outlet,Link} from "react-router-dom"
 import logo from '/logo.svg'
 
 function Nav() {
@@ -17,6 +17,7 @@ function Nav() {
                 <li><Link to='/image'>Image</Link></li>
             </ul>
         </div>
+        <Outlet />
     </nav>
   )
 }
