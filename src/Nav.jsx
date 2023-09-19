d743e00c8d4c39a4ba0ba5a3a8268645c5837d85
Pr Things <prthings@skiff.com>
2023-09-20 00:42:03 +0530

    added mobile menu

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
 import {Outlet,Link} from "react-router-dom"
 import logo from '/logo.svg'
 
 function Nav() {
+  const [mobile, setMobile]=useState(false)
+  const toggle=()=>{
+    setMobile(!mobile)
+  }
   return (
     <nav>
         <div className="container">
             <div className='logo'>
-              <a href="/"><img src={logo} alt="Logo" width={280} /></a>
+              <a href="/"><img src={logo} alt="Logo" width={230} /></a>
             {/* <h1>Hello<span>PR</span></h1> */}
             </div>
-            <ul id='menu'>
+            <ul className={mobile ? "menu mobmenu": "menu"}>
                 <li><Link to='/counter1'>Counter function</Link></li>
                 <li><Link to='/counter'>Counter</Link></li>
                 <li><Link to='/fetch'>Fetch</Link></li>
@@ -18,6 +23,9 @@ function Nav() {
             </ul>
         </div>
         <Outlet />
+        <div className='icons' onClick={toggle}>
+          {mobile ? (<i className='fas fa-times'></i>):(<i className='fas fa-bars'></i>)}
+        </div>
     </nav>
   )
 }
