01e56f5aa9f88b4ab16d6ddc2af413fb21e48ab5
Pr Things <prthings@skiff.com>
2023-09-20 15:12:55 +0530

    working quotes

--- src/Quotes.jsx
+++ src/Quotes.jsx
@@ -4,7 +4,11 @@ import { useState, useEffect } from 'react';
 function Quotes() {
     const [data, setData] = useState([]);
     useEffect(() => {
-      fetch('https://zenquotes.io/api/random', {mode: 'no-cors'})
+      fetch('https://api.api-ninjas.com/v1/quotes', {
+        headers:{
+          'X-Api-Key': 'mUlfVPsCfCDPS44qu9S9NQ==7vTdmvkDUp6bxk5q'
+        }
+      })
         .then(response => response.json())
         .then(data => setData(data))
         .catch(error => console.log(error))
@@ -13,8 +17,9 @@ function Quotes() {
     <div className='data'>
            <h1>Quote Generator From the API</h1>
            {data.map(items=>
-            <div key={items.c}>
-                <h2>{items.q}</h2>
+            <div className='quote' key={items.author}>
+                <h3>{items.quote}</h3>
+                <blockquote>-- {items.author}</blockquote>
             </div>
            )}
     </div>
