c9326d64c497a3d0e20d424e6a5d84f9d1f1a2a7
PR Things <89149952+prthings@users.noreply.github.com>
2023-09-23 09:28:31 +0530

    Update Freeapi.jsx

--- src/Freeapi.jsx
+++ src/Freeapi.jsx
@@ -1,1 +1,19 @@
+import React from 'react'
+import data from './data/api.json'
 
+function Freeapi() {
+    return (
+        <div className='main'>
+            {data.map(item => (
+               <div key={item.id} className='box'>
+                <h4>{item.id}</h4>
+                <h3>{item.category}</h3>
+                <h4>{item.description}</h4>
+                 <p><a href={item.url} target="_blank">{item.api-name}</a></p>
+               </div>
+            ))}
+        </div>
+      );
+}
+
+export default Freeapi
