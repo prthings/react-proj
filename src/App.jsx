a828d47d531dcd16f51972ee45c06c52ade9b5e5
PR Things <89149952+prthings@users.noreply.github.com>
2023-11-10 12:16:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
       <h1>Hello Home</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ut in atque, maxime incidunt, exercitationem quisquam consequatur odit sequi sapiente repellat debitis harum sit ab accusantium earum dolor facere rem.</p>
 
-<div className='home-button>
+<div className='home-button'>
       <button><Link to="/pass">Password Generator</Link></button>
       <button><Link to="/quote">Random Quotes</Link></button>
       <button><Link to="/image">Image</Link></button>
