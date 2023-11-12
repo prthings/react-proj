be91c4318f86077b8b98e4b4f5d94ae7167fe401
Pr Things <prthings@skiff.com>
2023-11-13 00:34:50 +0530

    updated content

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-
-
+import './assets/error.css'
 function Error() {
   return (
     <div className='main-section'>
       <div>
-            <h1>4<span style={{color:'red'}}>0</span>4</h1>
-            <h3 style={{textAlign:'center'}}>THE PAGE YOU REQUESTED COULD NOT FOUND</h3>
+            <h1>4<span className='zero'>0</span>4</h1>
+            <h3>The page you are looking for might have been removed had <br />its name changed or is temporarily unavailable.</h3>
             <a href="/"><button>Go Back</button></a>
       </div>
     </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='footer'>
-      <p style={{textAlign:'center',color:'red'}}>* Don&apos;t Refresh directly goto homapage and then try to refresh the page.</p>
+      <p className='info-warn'>Hi Welcome to all this is my first project that i am exapnding my skills through this project and i am learning from this project also and All my projects covered here</p>
         <hr />
         <div className='footer-content'>
         <div>© 2023 HelloPR, All rights reserved.</div>
