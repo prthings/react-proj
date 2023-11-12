abacd4a6f9934cda98592e3af4bd848abe54b27b
Pr Things <prthings@skiff.com>
2023-11-13 00:59:50 +0530

    styling error

--- src/Error.jsx
+++ src/Error.jsx
@@ -3,8 +3,9 @@ import './assets/error.css'
 function Error() {
   return (
     <div className='main-section'>
-      <div>
+      <div className='error-404'>
             <h1>4<span className='zero'>0</span>4</h1>
+            <div className='image-error'></div>
             <h3>The page you are looking for might have been removed had <br />its name changed or is temporarily unavailable.</h3>
             <a href="/"><button>Go Back</button></a>
       </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='footer'>
-      <p className='info-warn'>Hi Welcome to all this is my first project that i am exapnding my skills through this project and i am learning from this project also and All my projects covered here</p>
         <hr />
         <div className='footer-content'>
         <div>© 2023 HelloPR, All rights reserved.</div>
