c2451e583db1d647907b326e9c3a02affec5b755
Pr Things <prthings@skiff.com>
2023-11-13 01:59:51 +0530

    link added

--- src/Logos.jsx
+++ src/Logos.jsx
@@ -8,7 +8,7 @@ function Logos() {
                <div key={item.id} className='box'>
                 <h4>{item.shortname}</h4>
                 <h3>{item.name}</h3>
-                <div id='brand-logo'><img src={path} alt={item.brand} /></div>
+                <div id='brand-logo'><a href={item.url} target="_blank" rel="noopener noreferrer"><img src={path} alt={item.brand} /></a></div>
                 <h4>{item.founded}</h4>
                </div>
             ))}
