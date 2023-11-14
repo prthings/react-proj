a848ac7c484408526c232a3a9201b5b6c71b610c
Pr Things <prthings@skiff.com>
2023-11-14 13:09:50 +0530

    update

--- src/Logos.jsx
+++ src/Logos.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import data from './data/logos.json'
 function Logos() {
-    const path="./asset/logos/"
+
   return (
     <div className='main'>
         {data.map(item => (
                <div key={item.id} className='box'>
                 <h4>{item.shortname}</h4>
                 <h3>{item.name}</h3>
-                <div id='brand-logo'><a href={item.url} target="_blank" rel="noopener noreferrer"><img src={path} alt={item.brand} /></a></div>
+                <div id='brand-logo'><a href={item.url} target="_blank" rel="noopener noreferrer"><img src={imageList} alt={item.brand} /></a></div>
                 <h4>{item.founded}</h4>
                </div>
             ))}
